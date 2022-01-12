cbba3a6b279fb17b35a06f61bee8033cb2b8fa43
pdor <pdor92@hotmail.it>
2022-01-12 22:37:10 +0100

    Created "playRound" function.

--- script.js
+++ script.js
@@ -13,3 +13,32 @@ function computerPlay() {
             return "Scissors";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    function ston(s) {
+        switch (s.toLowerCase()) {
+            case "rock": return 0;
+            case "paper": return 1;
+            case "scissors": return 2;
+        }
+    }
+    function myMod(n, m) {
+        return ((n % m) + m) % m;
+    }
+    function capitalize(s) {
+        return s[0].toUpperCase() + s.slice(1);
+    }
+
+    let pn = ston(playerSelection);
+    let cn = ston(computerSelection);
+    let score = myMod(pn - cn, 3);
+
+    switch (score) {
+        case 0:
+            return "You Tie!"
+        case 1:
+            return `You Win! ${playerSelection} beats ${computerSelection}`;
+        case 2:
+            return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
